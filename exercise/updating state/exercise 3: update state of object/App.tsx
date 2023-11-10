ba2ec38a2f2d46efd133b4f037de503182eb2235
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-10 08:35:25 -0800

    Create App.tsx
    update state of object.

---
+++ exercise/updating state/exercise 3: update state of object/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+function App() {
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+    console.log(cart);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>click</button>
+    </div>
+  );
+}
+
+export default App;
