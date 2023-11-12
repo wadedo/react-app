905f4a207abd2bf973bcf9c15d1983b34fdbf5f3
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-12 08:11:54 -0800

    Create App.tsx

---
+++ exercise/keeping components pure/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Message from "./Message";
+
+function App() {
+  return (
+    <div>
+      <Message></Message>
+      <Message></Message>
+      <Message></Message>
+    </div>
+  );
+}
+
+export default App;
