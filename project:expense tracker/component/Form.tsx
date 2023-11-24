ccf63b35b418aade2934b601102e2470ba964cdb
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-24 07:34:14 -0800

    Update Form.tsx
    将提交表单时submit的数据 集中在一个函数 onSubmit里，因为在实际中不仅仅是获取数据这一个需求，可能会涉及更多的内容，这样我们就可以在一个独立的函数里实现这些需求。

--- project:expense tracker/component/Form.tsx
+++ project:expense tracker/component/Form.tsx
@@ -1,29 +1,21 @@
 import React from "react";
 import { FormEvent, useState } from "react";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
   // const form = useForm();
-  // console.log(form);
+
   const { register, handleSubmit } = useForm(); //register输出一个object。
 
-  //有了react hook form, 不需要state hook.
-  // const [person, setPerson] = useState({
-  //   name: "",
-  //   age: "",
-  // });
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          // value={person.name}
-          // onChange={(event) =>
-          //   setPerson({ ...person, name: event.target.value })
-          // }
           {...register("names")}
           id="name"
           type="text"
@@ -35,10 +27,6 @@ const Form = () => {
           Age
         </label>
         <input
-          // value={person.age}
-          // onChange={(event) =>
-          //   setPerson({ ...person, age: parseInt(event.target.value) })
-          // }
           {...register("age")}
           id="age"
           type="number"
