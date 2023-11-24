fa2ba52197ecb8d428350bb96c0008c834a4c765
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-23 23:57:33 -0800

    Update Form.tsx
    to make input to be an controlled component.

--- project:expense tracker/component/Form.tsx
+++ project:expense tracker/component/Form.tsx
@@ -1,19 +1,46 @@
 import React from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "", // anitialize to 0.
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(person);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          value={person.name} //make input to be controlled component.
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          id="name"
+          type="text"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          value={person.age} //make input to be controlled component.
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
