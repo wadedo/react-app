02088101d247b848b68b610bbd30ce57f1262f54
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-26 00:39:56 -0800

    Update Form.tsx
    从11.26号开始，重新过一遍，恢复到最开始的表单Form.即两个input和一个button。

--- project:expense tracker/component/Form.tsx
+++ project:expense tracker/component/Form.tsx
@@ -1,37 +1,19 @@
 import React from "react";
-import { FormEvent, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
 
 const Form = () => {
-  // const form = useForm();
-
-  const { register, handleSubmit } = useForm(); //register输出一个object。
-
-  const onSubmit = (data: FieldValues) => console.log(data);
-
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input
-          {...register("names")}
-          id="name"
-          type="text"
-          className="form-control"
-        />
+        <input id="name" type="text" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input
-          {...register("age")}
-          id="age"
-          type="number"
-          className="form-control"
-        />
+        <input id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
