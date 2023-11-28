2fa06a0707a868cf9834c1f029b0581d92e01fc0
wadedo <60915311+wadedo@users.noreply.github.com>
2023-11-28 07:03:05 -0800

    Create App.tsx

---
+++ project:expense tracker/expense-tracker/App.tsx
@@ -0,0 +1,22 @@
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
+
+function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <div>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      ></ExpenseList>
+    </div>
+  );
+}
+
+export default App;
