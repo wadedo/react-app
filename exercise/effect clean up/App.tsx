147b4525c27ffc192d07b52a74981764f82faa49
wadedo <60915311+wadedo@users.noreply.github.com>
2023-12-07 22:52:45 -0800

    Create App.tsx
    clean up or undo the effect.

---
+++ exercise/effect clean up/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
+
+function App() {
+  useEffect(() => {
+    connect();
+    return () => disconnect();
+  });
+  return <div></div>;
+}
+
+export default App;
