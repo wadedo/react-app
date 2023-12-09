14f3f70246fdeed43fb222423cf7aa594e38c22b
wadedo <60915311+wadedo@users.noreply.github.com>
2023-12-08 23:49:27 -0800

    Create App.tsx
    feching data from server using axios

---
+++ exercise/fetching data/App.tsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
