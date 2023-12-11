eb8bfad6c22420056e3bd2f2904f5ebdcda783af
wadedo <60915311+wadedo@users.noreply.github.com>
2023-12-10 23:20:50 -0800

    Create App.tsx
    use async and await

---
+++ exercise/async and await/App.tsx
@@ -0,0 +1,40 @@
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+    fetchUsers();
+
+    // .then((res) => setUsers(res.data))
+    // .catch((err) => setError(err.message));
+  }, []);
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
