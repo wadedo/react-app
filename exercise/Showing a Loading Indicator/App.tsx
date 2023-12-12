89962ba857dd67cc69094145af0986f8223848b3
wadedo <60915311+wadedo@users.noreply.github.com>
2023-12-12 01:07:28 -0800

    Create App.tsx

---
+++ exercise/Showing a Loading Indicator/App.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false); //adding.
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true); //adding
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false); //adding
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) return;
+        setError(err.message);
+        setLoading(false); //adding
+      });
+    
+
+    return () => controller.abort();
+  }, []);
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
