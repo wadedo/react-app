49e8bb71917f9390fc5cff456163734e90628afb
wadedo <60915311+wadedo@users.noreply.github.com>
2023-12-11 23:24:48 -0800

    Create App.tsx

---
+++ exercise/Cancelling a Fetch Request/App.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const controller = new AbortController();
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+        signal: controller.signal,
+      })
+      .then((res) => setUsers(res.data))
+      .catch((err) => {
+        if (axios.isCancel(err)) return;//important。 here is cancle error,所以直接return。
+        setError(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
